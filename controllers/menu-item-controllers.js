67a44a183be7bd20b766e71d6d555c7df244cfb2
afreen009 <shfafreen@gmail.com>
2024-07-14 01:17:28 -0400

    added get menu items api route

--- controllers/menu-item-controllers.js
+++ controllers/menu-item-controllers.js
@@ -4,13 +4,11 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 const getAllFoodItems = async (_req, res) => {
+  console.log("came here");
   try {
-    const menu_items = await knex("menu_items").join(
-      "cooks",
-      "cooks.id",
-      "menu_items.food_id"
-    );
-    // const menu_items = await knex("menu_items").select("*");
+    const menu_items = await knex("menu_items")
+      .join("cooks", "cooks.id", "menu_items.food_id")
+      .select("*");
     res.json(menu_items);
   } catch (err) {
     res.status(400).send(`Error retrieving list of menu: ${err}`);
